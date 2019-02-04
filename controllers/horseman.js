d7df4d99b9ec8669f80adc48a5c26b185b146a63
Yoonseok Oh <promiseys@hotmail.com>
2019-02-05 00:46:49 +0900

    Added exception case.

--- controllers/horseman.js
+++ controllers/horseman.js
@@ -2,16 +2,20 @@ module.exports = (function () {
   const Horseman = require('node-horseman');
 
   function getWebPageScreenShot(option) {
-    const horseman = new Horseman();
+    try {
+      const horseman = new Horseman();
 
-    return horseman.open(option.url.href)
-      .userAgent(option.userAgent)
-      .viewport(option.width, option.height)
-      .screenshot(`./public/images/${option.url.hostname}.jpg`)
-      .then(() => {
-        return `/images/${option.url.hostname}.jpg`;
-      })
-      .close();
+      return horseman.open(option.url.href)
+        .userAgent(option.userAgent)
+        .viewport(option.width, option.height)
+        .screenshot(`./public/images/${option.url.hostname}.jpg`)
+        .then(() => {
+          return `/images/${option.url.hostname}.jpg`;
+        })
+        .close();
+    } catch (e) {
+      return Promise.reject(e);
+    }
   }
 
   return {
--- controllers/puppeteer.js
+++ controllers/puppeteer.js
@@ -2,27 +2,31 @@ module.exports = (function () {
   const puppeteer = require('puppeteer');
 
   async function getWebPageScreenShot(option) {
-    // args for debian(ubuntu) dependencies
-    // refer: https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
+    try {
+      // args for debian(ubuntu) dependencies
+      // refer: https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md
+      const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      const page = await browser.newPage();
 
-    const viewport = {
-      width: option.width,
-      height: option.height
-    };
+      const viewport = {
+        width: option.width,
+        height: option.height
+      };
 
-    return page.setUserAgent(option.userAgent)
-      .then(() => {
-        return page.setViewport(viewport).then(async() => {
-          await page.goto(option.url.href);
-          await page.screenshot({path: `./public/images/${option.url.hostname}.jpg`});
+      return page.setUserAgent(option.userAgent)
+        .then(() => {
+          return page.setViewport(viewport).then(async() => {
+            await page.goto(option.url.href);
+            await page.screenshot({path: `./public/images/${option.url.hostname}.jpg`});
 
-          await browser.close();
+            await browser.close();
 
-          return Promise.resolve(`/images/${option.url.hostname}.jpg`);
-        })
-      });
+            return Promise.resolve(`/images/${option.url.hostname}.jpg`);
+          })
+        });
+    } catch (e) {
+      return Promise.reject(e);
+    }
   }
 
   return {
