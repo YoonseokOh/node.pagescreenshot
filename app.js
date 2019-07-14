a75247ad2028d6ef4edcfc4108191f94ec1f545b
Yoonseok Oh <promiseys@hotmail.com>
2019-07-14 17:31:28 +0900

    changed hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const exphbs = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,7 +11,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', exphbs({
+  defaultLayout: 'default',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
